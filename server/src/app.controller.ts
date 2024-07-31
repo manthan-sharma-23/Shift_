13b5f66a4c1ad56fd4c5c48168741abd89b388d8
Manthan Sharma <manthan2306s@gmail.com>
2024-07-31 18:15:36 +0530

    docker wathc

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/create-env')
+  async createUserContainer(@Req() req: Request) {
+    return await this.appService.createUserContainer(req);
   }
 }
--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Request } from 'express';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  public async createUserContainer(req: Request): Promise<string> {
+    const containerId = req.body.containerId as string;
+
+    return `New Container spinned up with id ${containerId}`;
   }
 }
