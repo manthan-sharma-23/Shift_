07d78ad9edbe4b45c6470dca4b4280d1d4f4b19b
Manthan Sharma <manthan2306s@gmail.com>
2024-08-01 18:15:16 +0530

    auth page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import AuthLayout from "./components/layouts/AuthLayout";
+import Signin from "./views/auth/Signin";
+import Signup from "./views/auth/Signup";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <RecoilRoot>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/auth" element={<AuthLayout />}>
+              <Route path="/auth/signin" element={<Signin />} />
+              <Route path="/auth/signup" element={<Signup />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </RecoilRoot>
+    </div>
+  );
 };
 
 export default App;
