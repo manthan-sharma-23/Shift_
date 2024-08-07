b334a2235d1a494e105e7fce92d271f088fa51f6
Manthan Sharma <manthan2306s@gmail.com>
2024-08-07 10:51:43 +0530

    c

--- server/src/api/cube-module/cube.module.ts
+++ server/src/api/cube-module/cube.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import CubeController from './cube.controller';
 import CubeService from './cube.service';
 import DatabaseService from 'src/engine/database/Database.service';
-import AuthenticateJWTToken from 'src/engine/core/guards/Authentication.guard';
 import { JwtService } from 'src/engine/core/services/Jwt.service';
 import { ContainerService } from 'src/engine/core/services/Container.service';
 
